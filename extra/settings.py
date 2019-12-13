7caed9314e5be34da232a5a2d7092fa5dc2b9915
davis <quilombomc2@gmail.com>
2019-12-13 10:52:33 -0600

    v3

--- extra/settings.py
+++ extra/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-CORS_ORIGIN_WHITELIST = ['ec2-54-173-168-100.compute-1.amazonaws.com', '54.173.168.100']
+CORS_ORIGIN_WHITELIST = ['ec2-54-202-67-207.us-west-2.compute.amazonaws.com', '54.202.67.207']
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
